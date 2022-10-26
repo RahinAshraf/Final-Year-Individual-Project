a72dd36329659e85c97b4bbae8f5c75d032bc5c8
RahinAshraf <k20034059@kcl.ac.uk>
2022-10-26 17:13:24 +0100

    Add Author name to README

--- models/itinerary.py
+++ models/itinerary.py
@@ -37,6 +37,8 @@ itinerary_data = []
 #
 # master_df.to_csv('Master Dataset.CSV')
 
+
+# add try-catch block for when data for the master is being loaded in
 with open('datasets/195JourneyDataExtract01Jan2020-07Jan2020 (1).csv', 'r') as csv_file:
     reader = csv.reader(csv_file)
     next(reader)  # skip over the field names of the csv files
